9a764793d3ffc2476609799001704f727f34ff15
sergio <scrummish@gmail.com>
2018-04-02 22:47:55 -0500

    posts index page now shows the users posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import _ from "lodash"
+
+import { connect } from  'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
+	renderPosts(){
+		return _.map(this.props.posts, (post) =>{
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			)
+		})
+	}
+
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
 	render(){
 		return(
-			<div> Posts Index Page </div>
+			<div> 
+				<ul>
+					{this.renderPosts()}
+				</ul>
+			</div>
 		);
 	};
 };
 
-export default PostsIndex;
+function mapStateToProps(state){
+	return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
