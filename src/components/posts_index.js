e6df790001982641fc7a3ce9cdb4fc4523f1b7a0
sergio <scrummish@gmail.com>
2018-04-02 21:27:23 -0500

    added react-router-dom and routed my index page

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+	render(){
+		return(
+			<div> Posts Index Page </div>
+		);
+	};
+};
+
+export default PostsIndex;
