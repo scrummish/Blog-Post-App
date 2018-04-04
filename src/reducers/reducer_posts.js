29bcedc9cd1f547c3d68126ffc9faa9dc498a4ca
sergio <scrummish@gmail.com>
2018-04-04 01:20:16 -0500

    Added comments to code

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action){
 	switch (action.type){
+		case DELETE_POST: // omit from lodash checks the state and omits any property that already exists in the state in its new state being returned
+			return _.omit(state, action.payload);
 		case FETCH_POST:
 			// Using key interpolation to create a new property in the state object 
 			// where the key is the ID and the data is the new post in order
