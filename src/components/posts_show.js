3ce61a2cbfc36444a5accfa9aca90b562039fe9e
sergio <scrummish@gmail.com>
2018-04-04 01:12:50 -0500

    CRUD blog post app complete using react, redux, react-router, and react-form

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
+import { deletePost } from '../actions';
 
 class PostsShow extends Component {
+	onDelete = ()=>{
+		const { id } = this.props.match.params;
+		
+		this.props.deletePost(id, ()=>{
+			// programatic navigation
+			this.props.history.push('/');
+		});
+	}
 	componentDidMount(){
 		// match.params is provided, naturally by react router in order to
 		// get access to the params in the url 
@@ -23,6 +32,9 @@ class PostsShow extends Component {
 		return(
 			<div> 
 				<Link to="/">Back To Index</Link>
+				<button className="btn btn-danger pull-xs-right" onClick={this.onDelete}>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<p>{post.content}</p>
 			</div>
@@ -36,4 +48,4 @@ function mapStateToProps({ posts }, ownProps) {
 	return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
